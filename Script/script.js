6b1c40065e74a04542d023099c6efb5caddbc057
dextortheclown <dexterwong0724@gmail.com>
2023-12-01 21:03:07 +0800

    added comments

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,12 @@
+// Selecting the navigation bar and menu button elements
 let navbar = document.querySelector('.header .navbar');
 let menu = document.querySelector('#menu-btn');
 
+// Toggling the mobile menu
 menu.onclick = () => {
+    // Toggle the 'fa-times' class on the menu button to change the icon
     menu.classList.toggle('fa-times');
+    // Toggle the 'active' class on the navigation bar to show/hide the mobile menu
     navbar.classList.toggle('active');
 }
 
@@ -13,6 +17,7 @@ document.querySelector('#close-form').onclick = () =>{
 /* home page carousell */
 var swiper = new Swiper(".home-slider", {
     grabCursor:true,
+    // Enabling loop for continuous slides
     loop:true,
     cnteredSlides:true,
     navigation: {
